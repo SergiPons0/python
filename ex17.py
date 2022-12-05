079ce0faeaee1f673885898b7b590a7880ca9720
SergiPons0 <spons20472@iesjoanramis.org>
2022-12-05 09:01:21 +0100

    Ejercficios py

---
+++ ex17.py
@@ -0,0 +1,10 @@
+def es_palindrom(a):
+    a=texto 
+    texto = input("Ingrese una palabra: ").lower()
+    rever = a[::-1]
+    if a == rever:
+        print("La palabra ingresada si es palindromo!!")
+    else:
+        print("La palabra ingresada no es palindromo!!")
+
+
---
+++ ex18.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+        first = [1,2,3,4]
+        second = [2,4,5,7]
+
+        common = set(first).intersection(second)
+
+print(common)
