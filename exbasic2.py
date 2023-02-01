31d52c4e6ba1486d04e26aecbb2d6f535a5f1e84
SergiPons0 <spons20472@iesjoanramis.org>
2023-02-01 13:43:46 +0100

    defafeaf

--- exbasic2.py
+++ exbasic2.py
@@ -1,17 +1,15 @@
-def llegir_llista(a):
+def llegir_llista():
     l = [] #Creamos una lista
     for i in range(10): #Hacemos un bucle de 10 elementos, 10 recorridos
         l.append(input()) #append--> afegeix al final de la llista
                             #input--> llegim del teclat
                             #input--> sempre retorna una cadena de caracters "" o ''
     return l
-    
-#Programa principal
-x = llegir_llista()
-suma = 0
-for e in x:
-    suma+= int(e)
-print("la suma es: ",suma)
 
-    
+x=llegir_llista()
+NumM = 0
+for e in x:
+    if e[0]== "M":
+        NumM+=1
+        print("Hay ", NumM, "Palabras que empiezan por M")
 
