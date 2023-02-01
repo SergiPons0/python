c2a9facc8e3812aba2d594d56f3ecbd2cf826c81
SergiPons0 <spons20472@iesjoanramis.org>
2023-02-01 13:23:25 +0100

    nnvsdvsd

---
+++ exbasic.py
@@ -0,0 +1,9 @@
+def llegir_llista(a):
+    l = [] #Creamos una lista
+    for i in range(10): #Hacemos un bucle de 10 elementos, 10 recorridos
+        l.append(input(x)) #append--> afegeix al final de la llista
+                            #input--> llegim del teclat
+
+    
+x = input("Insereix llista de nombres: ")
+
